893f94d64d1a547cf1dc8495d02c8ee964f54101
Code-Crafting <monojitsen90@gmail.com>
2025-09-28 20:11:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="text-7xl text-red-400">Hello World!</h1>;
+}
+
+export default App;
